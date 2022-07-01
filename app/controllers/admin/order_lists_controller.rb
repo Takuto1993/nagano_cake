1916c3bf4d1deea240a30fa94a1014f358e7a994
EC2 Default User <ec2-user@ip-172-31-40-91.ec2.internal>
2022-07-01 12:31:29 +0000

    [Add]受注ステータス実装

--- app/controllers/admin/order_lists_controller.rb
+++ app/controllers/admin/order_lists_controller.rb
@@ -1,2 +1,13 @@
 class Admin::OrderListsController < ApplicationController
+  def update
+    @order_list = OrderList.find(params[:id])
+    @order_list.update(order_params)
+    redirect_to admin_order_path(order.id)
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order_list).permit(:status)
+  end
 end
